f6a5a76feae3a1135f9dc2b5a28efdb967e5b232
zhangsan <zhangsan@itcast.cn>
2019-10-24 15:32:44 +0800

    把本地工程提交

--- itheima_git/src/main/java/Git.java
+++ itheima_git/src/main/java/Git.java
@@ -1,5 +1,11 @@
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
+        System.out.println("hello" );
     }
 }
